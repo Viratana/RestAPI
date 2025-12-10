9f33a803c26ad0207cddc9a076e985000e15a3ab
Viratana <viratanasithan67@gmail.com>
2025-12-10 15:58:11 +0700

    Data Manipulation with API

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Author;
+use Carbon\Carbon;
+use Dotenv\Validator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator as FacadesValidator;
 
 class AuthorController extends Controller
 {
@@ -12,7 +16,13 @@ class AuthorController extends Controller
      */
     public function index()
     {
-        //
+        $authors = Author::all();
+        if(!empty($authors)){
+            return Response::json(['data' => $authors], 200);
+        }
+        else{
+            return Response::json(['message' => 'No Record Found'], 404);
+        }
     }
 
     /**
@@ -28,7 +38,37 @@ class AuthorController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = [
+            'author_name'       => $request->author_name,
+            'author_contact_no' => $request->author_contact_no,
+            'author_country'    => $request->author_country,
+            'created_at'        => Carbon::now(),
+        ];
+
+        $rules = [
+            'author_name'       =>'required',
+            'author_contact_no' =>'required',
+            'author_country'    =>'required',
+        ];
+        $validator = FacadesValidator::make($request->all(), $rules);
+
+        if($validator->fails()){
+            return $validator->errors();
+        } else {
+            $author = Author::create($data);
+            
+            if($author){
+                return Response::json([
+                    'message' => "New Author has been created successfully!"
+                ], 200);
+            } else {
+                return Response::json([
+                    'message' => 'Something When Wrong'
+                ], 404);
+            }
+        }
+
+        
     }
 
     /**
@@ -36,7 +76,13 @@ class AuthorController extends Controller
      */
     public function show(Author $author)
     {
-        //
+        $authorr = Author::find($author->id);
+        if(!empty($authorr)){
+            return Response::json(['data' => $authorr], 200);
+        }
+        else{
+            return Response::json(['message' => 'No Record Found'], 404);
+        }
     }
 
     /**
@@ -52,7 +98,26 @@ class AuthorController extends Controller
      */
     public function update(Request $request, Author $author)
     {
-        //
+        // $data = [
+        //     'author_name'       => $request->author_name,
+        //     'author_contact_no' => $request->author_contact_no,
+        //     'author_country'    => $request->author_country,
+        //     'updated_at'        => Carbon::now(),
+        // ];
+
+        $auth = Author::find($author->id);
+        $auth->update($request->all());
+        // $auth = Author::where('id', $author->id)->update($data);
+
+        if($auth){
+            return Response::json([
+                'message' => "New Author has been updated successfully!"
+            ], 200);
+        } else {
+            return Response::json([
+                'message' => 'Something When Wrong'
+            ], 404);
+        }
     }
 
     /**
@@ -60,6 +125,27 @@ class AuthorController extends Controller
      */
     public function destroy(Author $author)
     {
-        //
+        $auth = Author::where('id', $author->id)->delete();
+
+        if($auth){
+            return Response::json([
+                'message' => "New Author has been deleted successfully!"
+            ], 200);
+        } else {
+            return Response::json([
+                'message' => 'Something When Wrong'
+            ], 404);
+        }
+    }
+
+    public function search($term){
+        // $authors = Author::where('author_name',​ $term)->get();       //search by full name
+        $authors = Author::where('author_name', "LIKE", "%" .$term. "%")->get();     //search by ចាប់តួអក្សរ       
+        if(!empty($authors)){
+            return Response::json(['data' => $authors], 200);
+        }
+        else{
+            return Response::json(['message' => 'No Record Found'], 404);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use function Laravel\Prompts\search;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('author', AuthorController::class);
+Route::get('author/search/{term}', [AuthorController::class, 'search']);
